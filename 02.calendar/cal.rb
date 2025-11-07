97a0d292dffc9b484366f8692fd9d539c96db722
kenji51 <kmori19941103@gmail.com>
2025-11-07 17:22:20 +0900

    カレンダープラクティスの修正版を提出

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,40 +1,46 @@
 #!/usr/bin/env ruby
 require 'date'
 require 'optparse'
+require 'debug'
 
 today = Date.today
 today_year = today.year
 today_month = today.month
-
-puts "#{today_month}月" " " "#{today_year}".center(20)
-puts "日 " "月 " "火 " "水 " "木 " "金 " "土 "
-
 params = ARGV.getopts("y:m:")
 
-if params["y"] == nil
+if params["y"] && params["m"]
+  puts "#{params["m"]}月 #{params["y"]}".center(20)
+elsif params["m"]
+  puts "#{params["m"]}月 #{today_year}".center(20)
+else
+  puts "#{today_month}月" " " "#{today_year}".center(20)
+end
+
+puts "日 月 火 水 木 金 土"
+
+if params["y"].nil?
   year = today_year
 else
   year = params["y"].to_i
 end
 
-if params["m"] == nil
+if params["m"].nil?
   month = today_month
 else
   month = params["m"].to_i
 end
 
 #カレンダーを表示させる
-first_day = Date.new(year, month, 1)
-last_day = Date.new(year, month, -1)
-print "   " * first_day.wday
+first_date = Date.new(year, month, 1)
+last_date = Date.new(year, month, -1)
+print "   " * first_date.wday
 
-(1..last_day.day).each do |number|
- day = Date.new(year, month, number)
- wday = day.wday
+(first_date..last_date).each do |date|
+  day_display = date.day.to_s.rjust(2) 
 
- if wday == 6
-    puts number.to_s.rjust(2) + " "
- else
-    print number.to_s.rjust(2) + " "
- end
+  if date.saturday?
+    puts day_display
+  else
+    print day_display + " " 
+  end
 end
