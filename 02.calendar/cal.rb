51d1ae926cd56fd2cce17ad0cc2036695e50842e
kenji51 <kmori19941103@gmail.com>
2025-11-10 21:43:52 +0900

    カレンダーのプラクティス修正版2回目を提出

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,46 +1,28 @@
 #!/usr/bin/env ruby
 require 'date'
 require 'optparse'
-require 'debug'
 
 today = Date.today
 today_year = today.year
 today_month = today.month
 params = ARGV.getopts("y:m:")
 
-if params["y"] && params["m"]
-  puts "#{params["m"]}月 #{params["y"]}".center(20)
-elsif params["m"]
-  puts "#{params["m"]}月 #{today_year}".center(20)
-else
-  puts "#{today_month}月" " " "#{today_year}".center(20)
-end
+year = params["y"].nil? ? today_year : params["y"].to_i
+month = params["m"].nil? ? today_month : params["m"].to_i
 
+puts "#{month}月  #{year}".center(20) 
 puts "日 月 火 水 木 金 土"
 
-if params["y"].nil?
-  year = today_year
-else
-  year = params["y"].to_i
-end
-
-if params["m"].nil?
-  month = today_month
-else
-  month = params["m"].to_i
-end
-
-#カレンダーを表示させる
 first_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
 print "   " * first_date.wday
 
 (first_date..last_date).each do |date|
-  day_display = date.day.to_s.rjust(2) 
+  day = date.day.to_s.rjust(2) 
 
   if date.saturday?
-    puts day_display
+    puts day
   else
-    print day_display + " " 
+    print day + " " 
   end
 end
