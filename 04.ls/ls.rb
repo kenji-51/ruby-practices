e8463201e0432eb8bed18caf00a08a752333d6bb
kenji51 <kmori19941103@gmail.com>
2026-06-29 17:58:53 +0900

    全体を綺麗にまとめた

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -7,6 +7,22 @@ COLS = 3
 
 list_cmd = false
 
+file_type_hash = {
+  '1' => '-',
+  '4' => 'd'
+}
+
+permission_hash = {
+  '0' => '---',
+  '1' => '--x',
+  '2' => '-w-',
+  '3' => '-wx',
+  '4' => 'r--',
+  '5' => 'r-x',
+  '6' => 'rw-',
+  '7' => 'rwx'
+}
+
 OptionParser.new do |opt|
   opt.on('-l') { list_cmd = true }
   opt.parse(ARGV)
@@ -55,72 +71,65 @@ def display_file_names(file_names, max_width)
   end
 end
 
-# メソッド
-if list_cmd
-  file_type_hash = {
-    '1' => '-',
-    '4' => 'd'
-  }
+def make_file_mode_structure(stat, file_type_hash, permission_hash)
+  mode = stat.mode.to_s(8)
 
-  permission_hash = {
-    '0' => '---',
-    '1' => '--x',
-    '2' => '-w-',
-    '3' => '-wx',
-    '4' => 'r--',
-    '5' => 'r-x',
-    '6' => 'rw-',
-    '7' => 'rwx'
-  }
+  object_type = mode.chars.first
+  file_type = file_type_hash[object_type]
 
+  file_permission = mode.chars.last(3)
+  file_mode = file_permission.map { |file_per| permission_hash[file_per] }.join('')
+
+  file_type + file_mode
+end
+
+def make_hardlinks(stat, files)
+  max_hardlink_width = files.map { |file| File.stat(file).nlink.to_s.length }.max
+  stat.nlink.to_s.rjust(max_hardlink_width)
+end
+
+def make_user(stat, files)
+  max_user_width = files.map { |file| Etc.getpwuid(File.stat(file).uid).name.length }.max
+  Etc.getpwuid(stat.uid).name.rjust(max_user_width)
+end
+
+def make_group(stat, files)
+  max_group_width = files.map { |file| Etc.getgrgid(File.stat(file).gid).name.length }.max
+  Etc.getgrgid(stat.gid).name.rjust(max_group_width)
+end
+
+def make_file_size(stat, files)
+  max_size_width = files.map { |file| File.stat(file).size.to_s.length }.max
+  stat.size.to_s.rjust(max_size_width)
+end
+
+def make_update_file_day(stat)
+  stat.mtime.strftime('%-b %e %H:%M')
+end
+
+def execute_file_list_and_total_blocks(files, file_type_hash, permission_hash)
   total_blocks = 0
-
-  def make_file_mode_structure(stat, file_type_hash, permission_hash)
-    mode = stat.mode.to_s(8)
-
-    object_type = mode.chars.first
-    file_type = file_type_hash[object_type]
-
-    file_permission = mode.chars.last(3)
-    file_mode = file_permission.map { |file_per| permission_hash[file_per] }.join('')
-
-    file_type + file_mode
-  end
-
-  def make_hardlinks(stat, files)
-    max_hardlink_width = files.map { |file| File.stat(file).nlink.to_s.length }.max
-    stat.nlink.to_s.rjust(max_hardlink_width)
-  end
-
-  def make_user(stat, files)
-    max_user_width = files.map { |file| Etc.getpwuid(File.stat(file).uid).name.length }.max
-    Etc.getpwuid(stat.uid).name.rjust(max_user_width)
-  end
-
-  def make_group(stat, files)
-    max_group_width = files.map { |file| Etc.getgrgid(File.stat(file).gid).name.length }.max
-    Etc.getgrgid(stat.gid).name.rjust(max_group_width)
-  end
-
-  def make_file_size(stat, files)
-    max_size_width = files.map { |file| File.stat(file).size.to_s.length }.max
-    stat.size.to_s.rjust(max_size_width)
-  end
-
-  def make_update_file_day(stat)
-    stat.mtime.strftime('%-b %e %H:%M')
-  end
 
   file_lists = files.map do |file|
     stat = File.stat(file)
     total_blocks += stat.blocks
 
-    [make_file_mode_structure(stat, file_type_hash, permission_hash), make_hardlinks(stat, files), make_user(stat, files), make_group(stat, files), make_file_size(stat, files), make_update_file_day(stat),
-     file].join(' ')
+    [
+      make_file_mode_structure(stat, file_type_hash, permission_hash),
+      make_hardlinks(stat, files),
+      make_user(stat, files),
+      make_group(stat, files),
+      make_file_size(stat, files),
+      make_update_file_day(stat),
+      file
+    ].join(' ')
   end
-
   puts "total #{total_blocks}"
   puts file_lists
+end
+
+if list_cmd
+  execute_file_list_and_total_blocks(files, file_type_hash, permission_hash)
 else
   file_names = build_file_names(files, COLS)
   max_width = make_max_width(file_names)
